200f6e331572f5189d6fa9e999d2d2433b2a7b22
realBeastSL <nadeeshh@outlook.com>
2022-06-26 12:00:29 +0530

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Nadeesh on 2022-06-26.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
